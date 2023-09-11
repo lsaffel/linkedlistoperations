d266d39817bd6661c81c278b913faad35793d3d8
Lisa <llvvss@icloud.com>
2023-09-11 13:40:53 -0600

    added insertAtEnd, adjusted printing, added new test code

--- main.py
+++ main.py
@@ -12,14 +12,27 @@ def insertAtFront(head_ptr, new_data):
     return new_node
 
 
+def insertAtEnd(head_ptr, new_data):
+    new_node = Node(new_data)
+
+    if head_ptr is None:
+        return new_node
+
+    temp_ptr = head_ptr
+    while temp_ptr.next is not None:
+        temp_ptr = temp_ptr.next
+
+    temp_ptr.next = new_node
+    return head_ptr
+
+
 def printLinkedList(head_ptr):
-    print("Printing linked list -------------------")
     temp_ptr = head_ptr
 
     while temp_ptr is not None:
         print(temp_ptr.data, end=" ")
         temp_ptr = temp_ptr.next
-
+    print("")
 
 if __name__ == '__main__':
     # Driver program
@@ -28,4 +41,26 @@ if __name__ == '__main__':
     head = insertAtFront(head, 7)
     head = insertAtFront(head, 34)
     head = insertAtFront(head, 19)
+    print("Printing linked list after first 3 elements added to front of list")
     printLinkedList(head)
+
+    print("Printing linked list after one more element added to end of list")
+    head = insertAtEnd(head, 25)
+    printLinkedList(head)
+
+    print("Printing linked list after one more element added to end of list")
+    head = insertAtEnd(head, 126)
+    printLinkedList(head)
+
+    head2 = None
+    print("Printing linked list that has no elements")
+    printLinkedList(head2)
+
+    head2 = insertAtFront(head2, 7)
+    print("Printing linked list that one element")
+    printLinkedList(head2)
+
+    head2 = None
+    head2 = insertAtEnd(head2, 115)
+    print("Printing linked list that one element")
+    printLinkedList(head2)
